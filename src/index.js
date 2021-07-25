3ba7054c82fa163290994081d18c628b0fa8518a
frank pagan <frank@cocreate.app>
2021-07-25 04:22:04 +0000

    fix: updated init and cleaned code

--- src/index.js
+++ src/index.js
@@ -8,123 +8,67 @@ import { logger } from '@cocreate/utils'
 let console = logger('off');
 
 const CoCreateText = {
-  elements: [],
 
-  init: function() {
-    this.initElement(document);
-    // this.initSockets();
-  },
+	selector: "input[data-collection][data-document_id][name], textarea[data-collection][data-document_id][name]",
 
-  refreshElement: function(mutation) {
-    let element = mutation.target;
-    if (!element) return;
-    const tagName = element.tagName.toLowerCase();
-    if (!['input', 'textarea'].includes(tagName) ||
-      !element.getAttribute('data-document_id') ||
-      !element.getAttribute('name')) {
-      return
-    }
-
-    if (!crud.isCRDT(element)) return;
-
-    // if (!observer.getInitialized(element, 'text')) {
-      this.__initEvents(element);
-    // }
-    // observer.setInitialized(element, 'text');
-
-    if (crud.checkAttrValue(element.getAttribute('data-document_id'))) {
-      // element.value = "";
-      this.createYDoc(element, true);
-    }
-
-  },
-
-  initElement: function(container) {
-    var self = this;
-    let fetch_container = container || document;
-
-    let elements = fetch_container.querySelectorAll('input[data-document_id][name], textarea[data-document_id][name]');
-    if (elements.length == 0 &&
-      fetch_container != document &&
-      fetch_container.hasAttribute('data-document_id') &&
-      fetch_container.hasAttribute('name')) {
-      elements = [fetch_container];
-    }
-
-    elements.forEach((element) => {
-      //ToDo: isCrdt Needs to be improved and located in crdt
-      // if (!crud.isCRDT(element)) {
-      //   return;
-      // }
-
-      if (self.checkExistElement(element)) {
-        // this.setInitValue(elements[i])
+	init: function() {
+		let elements = document.querySelectorAll(this.selector);
+		this.initElements(elements);
+	},
+	
+	initElements: function(elements) {
+		for (let element of elements)
+			this.initElement(element);
+	},
+	
+	initElement: function(element, data) {
+    if( element.tagName === "INPUT" &&  ["text", "email", "tel", "url"].includes(element.type) || element.tagName === "TEXTAREA"){
+  		const {collection, document_id, name, is_realtime} = crud.getAttr(element);
+  		if (!document_id || !is_realtime) return;
+  	
+  		if (document_id == 'pending') {
         return;
       }
-
-      // if (observer.getInitialized(element, 'text')) {
-      //   return;
-      // }
-      // observer.setInitialized(element, 'text');
-
-      self.__initEvents(element);
-
-      if (crud.checkAttrValue(element.getAttribute('data-document_id'))) {
-        self.createYDoc(element);
-
-      }
-      else {
-        //. register create document_id event
-        element.addEventListener('set-document_id', function(event) {
-          let el = this;
-          let text_str = el.value;
-
-          self.createYDoc(el);
-
-          el.value = "";
-          self.sendChangeData(el, text_str, 0, text_str.length);
-        })
-      }
-    })
-  },
-
-  checkExistElement: function(element) {
-    for (var i = 0; i < this.elements.length; i++) {
-      if (this.elements[i].isSameNode(element)) {
-        return true;
-      }
+      
+      element.setAttribute('crdt', 'true')
+      
+      this.__initEvents(element);
+     
+      this.createDoc(element);
+      
+      this.setValue(element)
     }
-    return false;
-  },
-
-  setInitValue: function(element) {
-    var typeId = this.generateTypeName(element);
-    var value = crdt.getWholeString(typeId);
-    element.value = value;
-
-    // input.setValue(element, value);
-  },
-
-  createYDoc: function(element, isExclude) {
+	},
+	
+	createDoc: function(element) {
     const { collection, document_id, name } = crud.getAttr(element)
     if (!crud.checkAttrValue(collection) || !crud.checkAttrValue(document_id) || !crud.checkAttrValue(name)) return;
-    const status = crdt.init({ collection, document_id, name, element })
-    if (!isExclude) {
-      this.elements.push(element)
-    }
-    else {
-      element.value = crdt.getText({ collection, document_id, name })
-      // input.setValue(element, crdt.getText({collection, document_id, name }));
-    }
+    
+  //   crdt.init({ collection, document_id, name, element }).then(data => {
+  //     element.value = crdt.getText({ collection, document_id, name })
+  //   }).catch(err => {
+		// 		console.error(err)
+		// })
+    crdt.init({ collection, document_id, name, element })
+	},
+	
+	setValue: function(element) {
+    var typeId = this.generateTypeName(element);
+    const { collection, document_id, name } = crud.getAttr(element)
+    element.value = crdt.getText({ collection, document_id, name })
+  },
+  
+  generateTypeName: function(element) {
+    const { collection, document_id, name } = crud.getAttr(element)
+    return crdt.generateID(config.organization_Id, collection, document_id, name);
   },
 
-
-  __initEvents: function(input_element) {
+  __initEvents: function(element) {
 
     //. selection event
     const self = this;
 
-    input_element.addEventListener('select', function() {
+    element.addEventListener('select', function() {
       if (this.selectionEnd !== this.selectionStart) {
         self.setSelectionInfo(this, true, this.selectionStart, this.selectionEnd)
 
@@ -133,14 +77,14 @@ const CoCreateText = {
       }
     });
 
-    input_element.addEventListener('keyup', function(event) {
+    element.addEventListener('keyup', function(event) {
       let arrows = [37, 38, 39, 40];
       if (arrows.indexOf(event.keyCode) != -1) {
         self.sendPosition(this);
       }
     });
 
-    input_element.addEventListener('keydown', function(event) {
+    element.addEventListener('keydown', function(event) {
       let arrows = [37, 38, 39, 40];
       if (arrows.indexOf(event.keyCode) != -1) {
         //console.log("keydown ---- ")
@@ -148,17 +92,17 @@ const CoCreateText = {
       }
     });
 
-    input_element.addEventListener('click', function(event) {
+    element.addEventListener('click', function(event) {
       if (document.activeElement === this)
         self.sendPosition(this);
     });
 
-    input_element.addEventListener('blur', function(event) {
+    element.addEventListener('blur', function(event) {
       const id = self.generateTypeName(this);
       crdt.setCursorNull(id);
     });
 
-    input_element.addEventListener('input', function(event) {
+    element.addEventListener('input', function(event) {
       let nowstart = this.selectionStart - 1;
       let nowend = nowstart;
       let selection_info = self.getSelectionInfo(this);
@@ -210,15 +154,15 @@ const CoCreateText = {
     })
 
     /** unselect events **/
-    input_element.addEventListener('blur', function(e) {
+    element.addEventListener('blur', function(e) {
       self.setSelectionInfo(this, false, this.selectionStart, this.selectionStart);
     })
-    input_element.addEventListener('click', function(e) {
+    element.addEventListener('click', function(e) {
       self.setSelectionInfo(this, false, this.selectionStart, this.selectionStart);
     })
 
     /** past events **/
-    input_element.addEventListener('paste', function(event) {
+    element.addEventListener('paste', function(event) {
       // return;
       console.log('check paste: text')
       let content_text;
@@ -242,9 +186,9 @@ const CoCreateText = {
       event.preventDefault()
     })
 
-    input_element.addEventListener('cocreate-crdt-update', function(event) {
+    element.addEventListener('cocreate-crdt-update', function(event) {
       var info = event.detail;
-      input_element.crudSetted = true;
+      element.crudSetted = true;
 
       var pos = 0;
       var flag = true;
@@ -288,22 +232,20 @@ const CoCreateText = {
       end: parseInt(e.getAttribute("selection_end"))
     }
   },
+  
+  sendPosition: function(element) {
+    const { collection, document_id, name } = crud.getAttr(element)
+    const id = this.generateTypeName(element);
+    let from = element.selectionStart;
+    let to = element.selectionEnd;
+    crdt.setPositionYJS(id, from, to);
+    // ToDo: use sendPosition
+    // crdt.sendPosition(collection, document_id, name, from, to);
+  },
 
-  // Todo: Add new crud
   sendChangeData: function(element, content, start, end, isRemove = true) {
     const { collection, document_id, name } = crud.getAttr(element)
     const isCrud = element.getAttribute('data-crud') == "false" ? false : true;
-
-    if (!document_id || document_id === "") {
-      // form.save(form)
-      // form.request({ element: element, nameAttr: "name" })
-      element.setAttribute('data-document_id', 'pending');
-      return;
-    }
-
-    if (document_id == 'pending') {
-      return;
-    }
 
     if (!crud.isSaveAttr(element)) {
       return;
@@ -372,103 +314,29 @@ const CoCreateText = {
     var isFocused = (document.activeElement === element);
     cursors.refresh_mirror(element);
 
-    // if (floatingLabel)   {
-    //   floatingLabel.update(element, element.value)
-    // }
-
   },
 
-  generateTypeName: function(element) {
-    const { collection, document_id, name } = crud.getAttr(element)
-
-    return crdt.generateID(config.organization_Id, collection, document_id, name);
-  },
-
-  setText: function(element_id, info) {
-    var pos = 0;
-    var flag = true;
-
-    info.forEach(item => {
-      if (item.retain) {
-        flag = true;
-        pos = item.retain;
-      }
-
-      if (item.insert || item.delete) {
-        if (flag == false) pos = 0;
-        flag = false;
-
-        if (item.insert) {
-          //. insert process
-          this.receiveChangeData(element_id, item.insert, pos, pos)
-        }
-        else if (item.delete) {
-          //. delete process
-          this.receiveChangeData(element_id, "", pos, pos + item.delete);
-        }
-
-      }
-    })
-  },
-
-  isAvaiableEl: function(element) {
-    //console.log(this.elements)
-
-    for (var i = 0; i < this.elements.length; i++) {
-      console.log(this.elements[i].isEqualNode(element), element)
-      if (this.elements[i].isEqualNode(element) === true) {
-        return true;
-      }
-    }
-    return false;
-  },
-
-  sendPosition: function(element) {
-    console.log("Se envio ")
-    /*if (!this.isAvaiableEl(element)) {
-      return;
-    }*/
-
-    const id = this.generateTypeName(element);
-    //console.log(" SEnd Position Selector ID ",id)
-    //console.log(element.selectionStart, element.selectionEnd);
-    let from = element.selectionStart;
-    let to = element.selectionEnd;
-    //console.log("Se envio la position ",id,from, to)
-    crdt.setPositionYJS(id, from, to);
-
-  }
 }
 
 
 CoCreateText.init();
-// CoCreateInit.register('CoCreateText', CoCreateText, CoCreateText.initElement);
+
 
 observer.init({
-  name: 'CoCreateTextCreate',
+  name: 'CoCreateTextAddedNodes',
 	observe: ['addedNodes'],
 	target: 'input[data-collection][data-document_id][name], textarea[data-collection][data-document_id][name]',
   callback: function(mutation) {
-
-    // console.log('cocreate-text init')
-    		let el = mutation.target;
-		el.hasAttribute('data-collection') &&
-		el.hasAttribute('data-document_id') &&
-		el.hasAttribute('name') &&
-    CoCreateText.initElement(mutation.target)
+    CoCreateText.initElement(mutation.addedNodes)
   }
 });
 
 observer.init({
-  name: 'CoCreateTextNameObserver',
+  name: 'CoCreateTextAttribtes',
   observe: ['attributes'],
 	attributeName: ['data-collection', 'data-document_id', 'name'],
   callback: function(mutation) {
-    // console.log('change cocreate-text name')
-    		mutation.target.hasAttribute('collection') &&
-    		mutation.target.hasAttribute('data-document_id') &&
-    		mutation.target.hasAttribute('name') &&
-    CoCreateText.refreshElement(mutation)
+    CoCreateText.initElement(mutation.target)
   }
 });
 
