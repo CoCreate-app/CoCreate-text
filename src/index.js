ad35c935426bdf82c5e861046939a6cdbf64a8df
frank pagan <frank@cocreate.app>
2021-08-05 22:47:13 +0000

    fix: dispatch input and keypress after crdt updates element

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import observer from '@cocreate/observer';
 import crud from '@cocreate/crud-client';
 import crdt from '@cocreate/crdt';
-
+let eventObj;
 const CoCreateText = {
 
     selector: "input[collection][document_id][name], textarea[collection][document_id][name]",
@@ -20,7 +20,7 @@ const CoCreateText = {
     initElement: function(element, data) {
         const { collection, document_id, name, isRealtime, isCrdt } = crud.getAttr(element);
         if(isCrdt == "false" || isRealtime == "false") return;
-        if (!crud.checkAttrValue(collection) && !crud.checkAttrValue(document_id)) return;
+        if(!crud.checkAttrValue(collection) && !crud.checkAttrValue(document_id)) return;
         if(element.tagName === "INPUT" && ["text", "email", "tel", "url"].includes(element.type) || element.tagName === "TEXTAREA") {
             if(!collection || !document_id || !name) return;
 
@@ -29,7 +29,7 @@ const CoCreateText = {
 
             this._addEventListeners(element);
 
-            crdt.init({collection, document_id, name});
+            crdt.init({ collection, document_id, name });
         }
     },
 
@@ -41,27 +41,32 @@ const CoCreateText = {
         element.addEventListener('paste', this._paste);
         element.addEventListener('keydown', this._keydown);
         element.addEventListener('keypress', this._keypress);
+        element.addEventListener('input', ()=>{
+            console.log('fff')
+        });
     },
-    
+
     _click: function(event) {
         let element = event.target;
         CoCreateText.sendPosition(element);
     },
-    
+
     _blur: function(event) {
         let element = event.target;
         const { collection, document_id, name } = crud.getAttr(element);
-        CoCreateText.sendPosition({ collection, document_id, name });
+        let start = null;
+        let end = null;
+        crdt.sendPosition({collection, document_id, name, start, end})
     },
-    
+
     _keyup: function(event) {
         let element = event.target;
         CoCreateText.sendPosition(element);
     },
-    
+
     _cut: function(event) {
         let element = event.target;
-        const {start, end} = CoCreateText.getSelections(element);
+        const { start, end } = CoCreateText.getSelections(element);
         const selection = document.getSelection();
         event.clipboardData.setData('text/plain', selection.toString());
         if(start != end) {
@@ -69,12 +74,11 @@ const CoCreateText = {
         }
         event.preventDefault();
     },
-    
+
     _paste: function(event) {
         let element = event.target;
         let value = event.clipboardData.getData('Text');
-        const {start, end} = CoCreateText.getSelections(element);
-
+        const { start, end } = CoCreateText.getSelections(element);
         if(start != end) {
             CoCreateText.deleteText(element, start, end);
         }
@@ -84,33 +88,38 @@ const CoCreateText = {
 
     _keydown: function(event) {
         let element = event.target;
-        const {start, end} = CoCreateText.getSelections(element);
         CoCreateText.sendPosition(element);
+        const { start, end } = CoCreateText.getSelections(element);
+        if(event.key == "Backspace" || event.key == "Tab" || event.key == "Enter") {
+            if(start != end) {
+                CoCreateText.deleteText(element, start, end);
+            }
+            if(event.key == "Backspace" && start == end) {
+                CoCreateText.deleteText(element, start - 1, end);
+            }
+            if(event.key == 'Tab') {
+                CoCreateText.insertText(element, "\t", start);
+            }
+            if(event.key == "Enter") {
+                CoCreateText.insertText(element, "\n", start);
+            }
+            event.preventDefault();
+
+        }
+    },
+
+    _keypress: function(event) {
+        if(event.stopCCText) return;
+        let element = event.target;
+        let { start, end } = CoCreateText.getSelections(element);
         if(start != end) {
             CoCreateText.deleteText(element, start, end);
         }
-        if (event.key == "Backspace" && start == end) {
-            CoCreateText.deleteText(element, start -1, end);
-            event.preventDefault();
-        }
-        if (event.key == 'Tab') {
-            CoCreateText.insertText(element, "\t", start);
-            event.preventDefault();
-        }
-        if (event.key == "Enter") {
-            CoCreateText.insertText(element, "\n", start);
-            event.preventDefault();
-        }
-    },
-   
-    _keypress: function(event) {
-        let element = event.target;
-        let {start} = CoCreateText.getSelections(element);
-        if (event.key == "Enter") return;
+        eventObj = event;
         CoCreateText.insertText(element, event.key, start);
         event.preventDefault();
     },
-    
+
     _removeEventListeners: function(element) {
         element.removeEventListener('click', this._click);
         element.removeEventListener('blur', this._blur);
@@ -127,23 +136,25 @@ const CoCreateText = {
             end: el.selectionEnd
         };
     },
-    
+
     sendPosition: function(el) {
+        if (!el) return;
+        const { start, end } = this.getSelections(el);
         const { collection, document_id, name } = crud.getAttr(el);
-        let start = el.selectionStart;
-        let end = el.selectionEnd;
-        crdt.sendPosition({collection, document_id, name, start, end});
+        // let start = el.selectionStart;
+        // let end = el.selectionEnd;
+        crdt.sendPosition({ collection, document_id, name, start, end });
     },
 
     deleteText: function(element, start, end) {
         const { collection, document_id, name, isCrud } = crud.getAttr(element);
-        let length = end - start; 
+        let length = end - start;
         crdt.deleteText({ collection, document_id, name, position: start, length, crud: isCrud });
     },
-    
+
     insertText: function(element, value, position) {
-        const { collection, document_id, name, isCrud, isSave} = crud.getAttr(element);
-        if (isSave == "false") return;
+        const { collection, document_id, name, isCrud, isSave } = crud.getAttr(element);
+        if(isSave == "false") return;
         crdt.insertText({ collection, document_id, name, value, position, crud: isCrud });
     },
 
@@ -154,18 +165,18 @@ const CoCreateText = {
             let collection = info['collection'];
             let document_id = info['document_id'];
             let name = info['name'];
-        	let selectors = `[collection='${collection}'][document_id='${document_id}'][name='${name}']`;
-        	let elements = document.querySelectorAll(`input${selectors}, textarea${selectors}`);
-        
-        	elements.forEach((element) => {
-                if(element === document.activeElement){
+            let selectors = `[collection='${collection}'][document_id='${document_id}'][name='${name}']`;
+            let elements = document.querySelectorAll(`input${selectors}, textarea${selectors}`);
+
+            elements.forEach((element) => {
+                if(element === document.activeElement) {
                     self.sendPosition(element);
                 }
-        		self._updateElement(element, info);
-        	});
+                self._updateElement(element, info);
+            });
         });
     },
-    
+
     _updateElement: function(element, info) {
         element.crudSetted = true;
 
@@ -213,16 +224,30 @@ const CoCreateText = {
                 prev_end = (prev_end >= end) ? prev_end - (end - start) : start;
             }
             element.selectionStart = prev_start;
-        } 
+        }
         else {
 
             element.selectionStart = prev_start;
             element.selectionEnd = prev_end;
         }
-       
-        if(element === document.activeElement){
+
+        if(element === document.activeElement) {
             this.sendPosition(element);
         }
+        
+        if(eventObj) {
+            let event = new CustomEvent(eventObj.type, { bubbles: true });
+            Object.defineProperty(event, 'stopCCText', { writable: false, value: true });
+            Object.defineProperty(event, 'target', { writable: false, value: eventObj.target });
+            element.dispatchEvent(event);
+           
+            let inputEvent = new CustomEvent('input', { bubbles: true });
+            Object.defineProperty(inputEvent, 'target', { writable: false, value: eventObj.target });
+            element.dispatchEvent(inputEvent);
+            eventObj = null;
+        }
+
+
     },
 };
 
