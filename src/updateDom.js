a73b1d9ad0b32819779b9aaaebec13176e5cab1c
Fran Pagan <frank@cocreate.app>
2021-11-23 00:16:32 +0000

    fix: handleing textNode and activeElement for curCaret

--- src/updateDom.js
+++ src/updateDom.js
@@ -7,81 +7,86 @@ export function updateDom({domTextEditor, value, start, end, html}) {
 		throw new Error('position is out of range');
     
     let {element, path, position, type} = getElementPosition(domTextEditor.htmlString, start, end);
-	
-	if (element) {
 		parseHtml(domTextEditor);
-		let domEl, newEl = element, oldEl, curCaret;
 		
-		if(element.tagName == 'HTML') {
-			domEl = domTextEditor;
-			curCaret = getSelection(domEl);
-			type = 'innerHTML';
-		}
-		else if(path) {
-			domEl = domTextEditor.querySelector(path);
-			oldEl = domTextEditor.oldHtml.querySelector(path);
-			curCaret = getSelection(domEl);
-		}
-		else {
-			curCaret = getSelection(domEl);
-		}
-		if (!value && type != 'isStartTag' && type != 'textNode'){
-			type = 'innerHTML';
-		}
+	let domEl, newEl = element, oldEl, curCaret;
+	if(!newEl){
+		newEl = domTextEditor.cloneNode(true);
+		newEl.innerHTML = html || domTextEditor.htmlString;
+		domEl = domTextEditor;
+		type = 'innerHTML';
+	}
+	else if(element.tagName == 'HTML') {
+		domEl = domTextEditor;
+		type = 'innerHTML';
+	}
+	else if(path) {
+		domEl = domTextEditor.querySelector(path);
+		oldEl = domTextEditor.oldHtml.querySelector(path);
+	}
+
+	let activeElement = domEl.ownerDocument.activeElement;
+	if (activeElement == domEl)
+		curCaret = getSelection(activeElement);
+	else if (activeElement.tagName == 'BODY')
+		curCaret = getSelection(domEl);
 		
-		if(domEl && newEl) {
-			if(start != end && type == 'innerHTML') {
-				domTextEditor.htmlString = html;
-				if (domEl.tagName != 'HTML')
+	if (!value && type != 'isStartTag' && type != 'textNode'){
+		type = 'innerHTML';
+	}
+	
+	if(domEl && newEl) {
+		if(start != end && type == 'innerHTML') {
+			domTextEditor.htmlString = html;
+			if (domEl.tagName != 'HTML'){
+				if (newEl.parentElement)
 					domEl.parentElement.replaceChildren(...newEl.parentElement.childNodes);
 				else
-					domEl.replaceChildren(...newEl.childNodes);	
-				domEl = newEl;
-				if (curCaret.range) {
-					curCaret.range.startContainer = domEl;
-					curCaret.range.endContainer = domEl;
-				}
+					domEl.replaceChildren(...newEl.childNodes);
 			}
-			else if (type == 'isStartTag')
-				assignAttributes(newEl, oldEl, domEl);
-			else if (type == 'insertAdjacent')
-				domEl.insertAdjacentHTML(position, value);
-			else if (type == 'textNode'){
-				if(start != end)
-					domTextEditor.htmlString = html;
-				domEl.innerHTML = newEl.innerHTML;
-			}
-			else if (type == 'innerHTML') {
-				domEl.replaceChildren(...newEl.childNodes);
+			else
+				domEl.replaceChildren(...newEl.childNodes);	
+			// domEl = newEl;
+			if (curCaret && curCaret.range) {
+				curCaret.range.startContainer = domEl;
+				curCaret.range.endContainer = domEl;
 			}
 		}
-		if(start && end) {
-	    	let p = processSelection(domEl, value, curCaret.start, curCaret.end, start, end, curCaret.range);
-	    	sendPosition(domEl);
-			_dispatchInputEvent(p.element, p.value, p.start, p.end, p.prev_start, p.prev_end);
+		else if (type == 'isStartTag')
+			assignAttributes(newEl, oldEl, domEl);
+		else if (type == 'insertAdjacent')
+			domEl.insertAdjacentHTML(position, value);
+		else if (type == 'textNode'){
+			if(start != end)
+				domTextEditor.htmlString = html;
+			domEl.innerHTML = newEl.innerHTML;
 		}
-		
-		if (newEl.tagName == 'HTML' || 'HEAD' || 'BODY' || 'SCRIPT'){
-			let scripts;
-			if (newEl.tagName == 'SCRIPT'){
-				scripts = [newEl]
-			}
-			else{
-				scripts = domEl.querySelectorAll('script');
-			}
-			for (let script of scripts) {
-				let newScript = domEl.ownerDocument.createElement('script');
-				for(let attribute of script.attributes) {
-					newScript.setAttribute(attribute.name, attribute.value);
-				}
-				newScript.innerHTML = script.innerHTML;
-				script.replaceWith(newScript);
-			}	
+		else if (type == 'innerHTML') {
+			domEl.replaceChildren(...newEl.childNodes);
 		}
 	}
-	else {
-		element = domTextEditor;
-		element.innerHTML = value;
+	if(curCaret && start && end) {
+    	let p = processSelection(domEl, value, curCaret.start, curCaret.end, start, end, curCaret.range);
+    	sendPosition(domEl);
+		_dispatchInputEvent(p.element, p.value, p.start, p.end, p.prev_start, p.prev_end);
+	}
+	
+	if (newEl.tagName == 'HTML' || 'HEAD' || 'BODY' || 'SCRIPT'){
+		let scripts;
+		if (newEl.tagName == 'SCRIPT'){
+			scripts = [newEl];
+		}
+		else{
+			scripts = domEl.querySelectorAll('script');
+		}
+		for (let script of scripts) {
+			let newScript = domEl.ownerDocument.createElement('script');
+			for(let attribute of script.attributes) {
+				newScript.setAttribute(attribute.name, attribute.value);
+			}
+			newScript.innerHTML = script.innerHTML;
+			script.replaceWith(newScript);
+		}	
 	}
 }
 
