1b7a433acda72f4cf12ad283b6aa37b4367fbea5
Fran Pagan <frank@cocreate.app>
2022-01-14 18:24:45 +0000

    fix: saveDomText.js updated to awiat value from crdt.getText

--- src/saveDomText.js
+++ src/saveDomText.js
@@ -5,21 +5,22 @@ import crdt from '@cocreate/crdt';
 
 function save(btn){
 	const { collection, document_id, name, namespace, room, isBroadcast, isBroadcastSender, isUpsert} = crud.getAttr(btn);
-	let data = crdt.getText({collection, document_id, name});
-	crud.updateDocument({
-		collection: collection,
-		document_id: document_id,
-		data: {
-			[name]: data
-		},
-		upsert: isUpsert,
-		namespace: namespace,
-		room: room,
-		broadcast: isBroadcast,
-		broadcast_sender: isBroadcastSender
+	crdt.getText({collection, document_id, name}).then(response => {
+		crud.updateDocument({
+			collection: collection,
+			document_id: document_id,
+			data: {
+				[name]: response
+			},
+			upsert: isUpsert,
+			namespace: namespace,
+			room: room,
+			broadcast: isBroadcast,
+			broadcast_sender: isBroadcastSender
+		});
+		
+		document.dispatchEvent(new CustomEvent('savedDomText'));
 	});
-	
-	document.dispatchEvent(new CustomEvent('savedDomText'));
 }
 
 action.init({
